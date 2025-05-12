2dc43f6fc5f3ecd6fb5cf1b6c3131ce7ef9a4e00
kovalevska olha <146865293+KovalevskaOlga@users.noreply.github.com>
2025-05-12 14:28:57 +0300

    Updates

---
+++ HW-JS-6.f8f65a59.js
@@ -0,0 +1,2 @@
+document.getElementById("btn").addEventListener("click",()=>{var t;t=function(){let t="#";for(let e=0;e<6;e++)t+="0123456789ABCDEF"[Math.floor(16*Math.random())];return t}(),document.body.style.backgroundColor=t});
+//# sourceMappingURL=HW-JS-6.f8f65a59.js.map
